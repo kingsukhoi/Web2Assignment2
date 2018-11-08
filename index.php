7e9311b87f3ad83b9e2c6f69130c22522a1ae237
gio <giohannym@preaxia.com>
2018-11-08 04:17:34 -0700

    "Created the about page as well as the new nav- In development Page"

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
+
 
 <?php
-include 'inc/header.inc'
+include 'development.php'
 ?>
-<br>
-<h1>Page in Development- see <a class="link" href="about.php">About</a> page to learn more</h1>
-</body>
