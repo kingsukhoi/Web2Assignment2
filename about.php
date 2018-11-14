82b237a51a8f34841b04f8e81f07eacc87ccb4f0
gio <gio@macmoro.com>
2018-11-14 13:36:49 -0700

    "fixed menu for both, mobile and desktop"

--- about.php
+++ about.php
@@ -6,30 +6,36 @@ include 'inc/header.inc'
 ?>
 <link rel="stylesheet" href="css/style.css">
 
-<br>
 
+<div class="row"></div>
 <div class="row">
+    <h1>About</h1>
     <div class="column3">
-        <h1>Farsos Bursara</h1>
+        <h1>Farsos Bulsara</h1>
         <img src="images/farsus.png">
         <p> Backbone Machine</p>
-        <p><img  src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/kingsukhoi">Git Account</a></p>
+        <p><img src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/kingsukhoi">Git
+                Account</a></p>
     </div>
     <div class="column3">
-        <h1>Nyk</h1>
+        <h1>Nyk Segunda</h1>
 
         <img src="images/nik.png">
         <p class=""> Jack of All Trades</p>
-        <p><img  src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/mseku953">Git Account</a></p>
+        <p><img src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/mseku953">Git
+                Account</a></p>
     </div>
     <div class="column3">
-        <h1 >Gio Moros</h1>
+        <h1>Gio Moros</h1>
         <img src="images/gio.png">
         <p> UI/UX Guy</p>
-        <p><img  src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/gmoro646">Git Account</a> </p>
+        <p><img src="images/git.png" width="26" height="26"><a class="link" href="https://github.com/gmoro646">Git
+                Account</a></p>
     </div>
 
 </div>
+
+
 </body>
 </html>
 <span></span>
--- development.php
+++ development.php
@@ -5,8 +5,16 @@
 <?php
 include 'inc/header.inc'
 ?>
-<div class="container">
-    <h1>Page in Development- see <a class="link" href="about.php">About</a> page to learn more</h1>
+<div class="row">
+    <div class="column">
+
+        <h1 class="eleven columns">Page in Development- see <a class="link" href="about.php">About</a> page to learn more</h1>
+    </div>
+
+
+
 </div>
+
+
 
 </body>
