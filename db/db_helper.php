a19476d740628620a671dc0b5a44ee6585049b59
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-15 09:40:59 -0700

    added db configuration and basic helper methods

---
+++ db/db_helper.php
@@ -0,0 +1,45 @@
+<?php
+/*
+  This function returns a connection object to a database
+*/
+
+include 'config.inc.php';
+
+function newConnection(){
+    return setConnectionInfo(DBCONNSTRING, DBUSER, DBPASS);
+}
+
+function setConnectionInfo( $connString, $user, $password ) {
+    $pdo = new PDO($connString,$user,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
+
+/*
+  This function runs the specified SQL query using the
+  passed connection and the passed array of parameters (null if none)
+*/
+function runQuery(PDO $connection, $sql, $parameters=array())     {
+    // Ensure parameters are in an array
+    if (!is_array($parameters)) {
+        $parameters = array($parameters);
+    }
+
+    $statement = null;
+    if (count($parameters) > 0) {
+        // Use a prepared statement if parameters
+        $statement = $connection->prepare($sql);
+        $executedOk = $statement->execute($parameters);
+        if (! $executedOk) {
+            throw new PDOException;
+        }
+    } else {
+        // Execute a normal query
+        $statement = $connection->query($sql);
+        if (!$statement) {
+            throw new PDOException;
+        }
+    }
+    return $statement;
+}
+
