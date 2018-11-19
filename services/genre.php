c7ef695119f064a6046c51e2e714f9bb8636af6d
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-19 14:14:34 -0700

    added genre

--- services/genre.php
+++ services/genre.php
@@ -2,3 +2,22 @@
 /*with no parameter, return JSON representation of all genres.
 If supplied with id parameter, then return just JSON data for single specified genre
 */
+
+include "../db/db_helper.php";
+include "../db/data_helper.php";
+include "../json/json_helper.php";
+
+header('Content-Type: application/json');
+
+
+$connection = newConnection();
+
+$id = "";
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+}
+$paramList = 'GenreID,GenreName,EraID,Description,Link
+';
+echo pdoStmtToJson(getData($connection, $id,"GenreID", $paramList, 'art.Genres'));
+
+$connection = null;
