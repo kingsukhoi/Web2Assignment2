09466ac7287bfca5f7abf9967c61c7fb5c9fe149
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-19 14:12:46 -0700

    switch these 2 to use my data helper.

--- services/artist.php
+++ services/artist.php
@@ -1,31 +1,12 @@
 <?php
 include "../db/db_helper.php";
 include "../json/json_helper.php";
+include '../db/data_helper.php';
 /*
 with no parameter, return JSON representation of all artists.
 If supplied with id parameter, then return just JSON data for single specified artist.
 */
 header('Content-Type: application/json');
-
-/**
- * @param PDO $connection
- * @param string $id Artist id. If not set pass empty string
- * @return bool|PDOStatement
- */
-function getArtist(PDO $connection, string $id){
-    $sql = '
-SELECT ArtistID,FirstName,LastName,Nationality,Gender,YearOfBirth,YearOfDeath,Details,ArtistLink 
-FROM art.Artists
-';
-    // truthy falsy baby
-    if ($id){
-        $sql .= 'WHERE ArtistID = :id';
-    }
-    $stmt = $connection -> prepare($sql);
-    $stmt -> bindValue(':id', $id);
-    $stmt -> execute();
-    return $stmt;
-}
 
 $connection = newConnection();
 
@@ -34,7 +15,8 @@ if (isset($_GET['id'])){
     $id = $_GET['id'];
 }
 
+$paramList = "ArtistID,FirstName,LastName,Nationality,Gender,YearOfBirth,YearOfDeath,Details,ArtistLink";
 
-echo pdoStmtToJson(getData($connection, $id));
+echo pdoStmtToJson(getData($connection, $id,"ArtistID" ,$paramList, "art.Artists"));
 
 $connection = null;
--- services/gallery.php
+++ services/gallery.php
@@ -3,3 +3,22 @@
 with no parameter, return JSON representation of all galleries.
 If supplied with id parameter, then return just JSON data for single specified gallery.
 */
+
+include "../db/db_helper.php";
+include "../db/data_helper.php";
+include "../json/json_helper.php";
+
+header('Content-Type: application/json');
+
+
+$connection = newConnection();
+
+$id = "";
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+}
+$paramList = 'GalleryID,GalleryName,GalleryNativeName,GalleryCity,GalleryAddress,GalleryCountry,Latitude,Longitude,GalleryWebSite,FlickrPlaceID,YahooWoeID,GooglePlaceID
+';
+echo pdoStmtToJson(getData($connection, $id,"GalleryID", $paramList, 'art.Galleries'));
+
+$connection = null;
