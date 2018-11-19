22c57e941e0f6b0d45b5eede8450eff0d00a7995
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-19 14:00:38 -0700

    added phpdoc comments

--- db/db_helper.php
+++ db/db_helper.php
@@ -1,24 +1,32 @@
 <?php
-/*
-  This function returns a connection object to a database
-*/
-
 include 'config.inc.php';
-
+/**
+ * @return PDO New Connection all setup and ready to go
+ */
 function newConnection(){
     return setConnectionInfo(DBCONNSTRING, DBUSER, DBPASS);
 }
 
+/** This function returns a connection object to a database
+ * @param $connString
+ * @param $user
+ * @param $password
+ * @return PDO
+ */
 function setConnectionInfo( $connString, $user, $password ) {
     $pdo = new PDO($connString,$user,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
 }
 
-/*
-  This function runs the specified SQL query using the
-  passed connection and the passed array of parameters (null if none)
-*/
+/**
+ * This function runs the specified SQL query using the
+ * passed connection and the passed array of parameters (null if none)
+ * @param PDO $connection
+ * @param $sql
+ * @param array $parameters
+ * @return bool|null|PDOStatement
+ */
 function runQuery(PDO $connection, $sql, $parameters=array())     {
     // Ensure parameters are in an array
     if (!is_array($parameters)) {
