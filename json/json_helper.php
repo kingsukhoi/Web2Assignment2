2ac389277af39ecbdc6af7701cf8b828ffc1ba17
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-20 11:16:31 -0700

    updated comment and message

--- json/json_helper.php
+++ json/json_helper.php
@@ -1,13 +1,14 @@
 <?php
 /**
  * @param PDOStatement $stmt pdo statement that has been executed
- * @return false|string
+ * @return false|string json representation of pdo stmt. If there is no data in pdo stmt, it set's the header
+ * to 404 and returns an error response
  */
 function pdoStmtToJson(PDOStatement $stmt){
     $data = $stmt -> fetchAll(PDO::FETCH_ASSOC);
     if (count($data) == 0){
         header("HTTP/1.0 404 Not Found");
-        return json_encode(['error' => 'page not found']);
+        return json_encode(['error' => 'id not found']);
     }
     return json_encode($data);
 }
