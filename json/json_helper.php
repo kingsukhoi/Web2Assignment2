36c4412c7d5a8875cc36064452501b7794e148aa
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-20 10:50:55 -0700

    added todo

--- json/json_helper.php
+++ json/json_helper.php
@@ -4,5 +4,6 @@
  * @return false|string
  */
 function pdoStmtToJson(PDOStatement $stmt){
+    //todo add 404 if there is no data, via exception
     return json_encode($stmt -> fetchAll(PDO::FETCH_ASSOC));
 }
