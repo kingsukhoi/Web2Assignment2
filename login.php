7e2a5c65ac6373798c4edd08444aff9f6adf7bb3
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-26 10:23:22 -0700

    added names

--- login.php
+++ login.php
@@ -9,11 +9,11 @@ include 'components/nav.php'
         <div class="row">
             <div class="five columns">
                 <label for="Username">Username</label>
-                <input class="u-full-width" type="text" placeholder="Username" id="Username">
+                <input class="u-full-width" name="username" type="text" placeholder="Username" id="Username">
             </div>
             <div class="five columns">
                 <label for="Username">Password</label>
-                <input class="u-full-width" type="password" placeholder="Password" id="Password">
+                <input class="u-full-width" name="password" type="password" placeholder="Password" id="Password">
             </div>
             <div class="two columns">
                 <label for="exampleEmailInput">&nbsp;</label>
