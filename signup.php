7f227eac11749896d594fd45600aa07daaa13d33
Farsos Bulsara <kingsukhoi@gmail.com>
2018-11-27 07:09:02 -0700

    added form method and misc html stuff

--- signup.php
+++ signup.php
@@ -1,35 +1,42 @@
+<html lang="en">
 <?php
 include 'inc/header.inc'
 ?>
+
+<body>
 <?
 include 'components/nav.php'
 ?>
 <div id="login" class="container">
-    <form>
+    <form action="services/signup.php" method="post">
         <div class="row">
             <div class="five columns">
                 <label for="Username">Firstname</label>
-                <input class="u-full-width" type="text" placeholder="firstname" id="firstname">
+                <input class="u-full-width" name="firstname" type="text" placeholder="First Name" id="firstname">
             </div>
             <div class="five columns">
                 <label for="Username">Last name</label>
-                <input class="u-full-width" type="text" placeholder="lastname" id="lastname">
+                <input class="u-full-width" name="lastname" type="text" placeholder="Last Name" id="lastname">
             </div>
             <div class="five columns">
-                <label for="Username">Last name</label>
-                <input class="u-full-width" type="text" placeholder="lastname" id="lastname">
+                <label for="city">City</label>
+                <input class="u-full-width" name="city" type="text" placeholder="City" id="city">
             </div>
             <div class="five columns">
-                <label for="Username">Last name</label>
-                <input class="u-full-width" type="text" placeholder="lastname" id="lastname">
+                <label for="country">Country</label>
+                <input class="u-full-width" name="country" type="text" placeholder="Country" id="country">
             </div>
             <div class="five columns">
-                <label for="Username">Last name</label>
-                <input class="u-full-width" type="text" placeholder="lastname" id="lastname">
+                <label for="email">Email</label>
+                <input class="u-full-width" name="email" type="text" placeholder="Email" id="email">
             </div>
             <div class="five columns">
-                <label for="Username">Password</label>
-                <input class="u-full-width" type="password" placeholder="Password" id="Password">
+                <label for="pass1">Password</label>
+                <input class="u-full-width" name="password" type="password" placeholder="Password" id="pass1">
+            </div>
+            <div class="five columns">
+                <label for="pass2">Repeat Password</label>
+                <input class="u-full-width" type="password" placeholder="Repeat Password" id="pass2">
             </div>
             <div class="two columns">
                 <label for="exampleEmailInput">&nbsp;</label>
@@ -38,5 +45,6 @@ include 'components/nav.php'
         </div>
     </form>
 </div>
+</body>
 
-
+</html>
