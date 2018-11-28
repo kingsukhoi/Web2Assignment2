3c553f252a10fcdad2d9f737378a0f006a00e2a4
Nick Sekunda <mseku953@mtroyal.ca>
2018-11-28 13:50:45 -0700

    This is for image generation, call it with a query string, required qstring parameters for it: width - desired size of image. source - the file path to the image. pic - the name of the jpeg file.

---
+++ make-image.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * This will return a php generated image when you pass the file name/number, the file path, and the desired width in
+ * the query string.
+ */
+    $pic = $_GET['file'];
+    $source = $_GET['type'];
+    $width = $_GET['width'];
+
+    $imgSource = "images/$source/$pic.jpg";
+
+    header('Content-Type: image/jpeg');
+
+    $img = imagecreatefromjpeg($imgSource);
+
+    $newImg = imagescale($img, $width, $width);
+
+    imagejpeg($newImg);
+
