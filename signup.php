692157a6f32b7aa191d1f45d52636cb7fd5b7c2f
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-03 13:06:20 -0700

    added builtin html validation. Works for most things with minimal Javascript. Me LIKEEEE

--- signup.php
+++ signup.php
@@ -12,32 +12,34 @@ include 'components/nav.php'
         <div class="row">
             <div class="five columns">
                 <label for="Username">Firstname</label>
-                <input class="u-full-width" name="firstname" type="text" placeholder="First Name" id="firstname">
+                <input class="u-full-width" name="firstname" type="text" placeholder="First Name" id="firstname" required>
             </div>
             <div class="five columns">
                 <label for="Username">Last name</label>
-                <input class="u-full-width" name="lastname" type="text" placeholder="Last Name" id="lastname">
+                <input class="u-full-width" name="lastname" type="text" placeholder="Last Name" id="lastname" required>
             </div>
             <div class="five columns">
                 <label for="city">City</label>
-                <input class="u-full-width" name="city" type="text" placeholder="City" id="city">
+                <input class="u-full-width" name="city" type="text" placeholder="City" id="city" required>
             </div>
             <div class="five columns">
                 <label for="country">Country</label>
-                <input class="u-full-width" name="country" type="text" placeholder="Country" id="country">
+                <input class="u-full-width" name="country" type="text" placeholder="Country" id="country" required>
             </div>
             <div class="five columns">
                 <label for="email">Email</label>
-                <input class="u-full-width" name="email" type="text" placeholder="Email" id="email">
+                <input class="u-full-width" name="email" type="email" placeholder="Email" id="email" required
+                pattern="(?:[a-z0-9!#$%&\\x27*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\\x27*+/=?^_`{|}~-]+)*|\x22(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\x22)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+                title="Please Enter a Valid Email">
 
             </div>
             <div class="five columns">
                 <label for="pass1">Password</label>
-                <input class="u-full-width" name="password" type="password" placeholder="Password" id="pass1">
+                <input class="u-full-width" name="password" type="password" placeholder="Password" id="password" required>
             </div>
             <div class="five columns">
                 <label for="pass2">Repeat Password</label>
-                <input class="u-full-width" type="password" placeholder="Repeat Password" id="pass2">
+                <input class="u-full-width" type="password" placeholder="Repeat Password" id="confirm-password" required>
             </div>
             <div class="two columns">
                 <label for="exampleEmailInput">&nbsp;</label>
