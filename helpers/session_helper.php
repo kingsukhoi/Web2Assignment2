5ecfbabb608fa6eb2110a5407a45029bfb04a3c3
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-05 13:45:10 -0700

    added session started method and used it for error checking.

--- helpers/session_helper.php
+++ helpers/session_helper.php
@@ -9,6 +9,9 @@
  * @param string $CustomerID id of the customer this session belongs to
  */
 function StartUserSession(string $CustomerID){
+    if(SessionStarted()){
+        return;
+    }
     $success = session_start();
     if (!$success)
         die(1);
@@ -19,5 +22,15 @@ function StartUserSession(string $CustomerID){
  * @return bool
  */
 function EndUserSession(){
+    if (!SessionStarted()){
+        return;
+    }
     return session_destroy();
+}
+
+/** check if there is an existing session
+ * @return bool True if session exists.
+ */
+function SessionStarted(){
+    return session_id() == '' || !isset($_SESSION);
 }
