5d074866c0b2e2f44800b1e71a27c5ce219098e1
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-07 12:05:49 -0700

    added required fields

--- login.php
+++ login.php
@@ -11,11 +11,13 @@ include 'components/nav.php'
             <div class="five column error"><?echo isset($_GET['error'])?'Error: '. htmlentities($_GET['error']):''?></div>
             <div class="five columns">
                 <label for="Username">Username</label>
-                <input class="u-full-width" name="username" type="text" placeholder="Username" id="Username">
+                <input class="u-full-width" name="email" type="email" placeholder="Email" id="email" required
+                       pattern="(?:[a-z0-9!#$%&\\x27*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\\x27*+/=?^_`{|}~-]+)*|\x22(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\x22)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+                       title="username@provider.com">
             </div>
             <div class="five columns">
                 <label for="Username">Password</label>
-                <input class="u-full-width" name="password" type="password" placeholder="Password" id="Password">
+                <input class="u-full-width" name="password" type="password" placeholder="Password" id="Password" required>
             </div>
             <div class="two columns">
                 <label for="exampleEmailInput">&nbsp;</label>
