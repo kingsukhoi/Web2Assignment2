2d366d0fffe56614c360e2975a6d31a2a0137018
Nick Sekunda <mseku953@mtroyal.ca>
2018-12-07 16:51:26 -0700

    Playing around, currently trying to figure out what I have to work with v0.2

--- single-gallery.php
+++ single-gallery.php
@@ -10,40 +10,6 @@ include "inc/session.inc.php";
 <body>
 <?php include 'components/nav.php' ?>
 
-
-
-<?
-/**
- * I'm gonna clean this code up later and it actually wont be on this page,
- * just wanna get shit working for now.
- */
-
-include "../db/db_helper.php";
-include "../db/data_helper.php";
-include "../json/json_helper.php";
-
-include "../inc/json.inc.php";
-
-
-$connection = newConnection();
-
-$id = "";
-if (isset($_GET['id'])){
-    $id = $_GET['id'];
-}
-
-$paramList = 'GalleryID,GalleryName,GalleryNativeName,GalleryCity,GalleryAddress,GalleryCountry,Latitude,Longitude,GalleryWebSite,FlickrPlaceID,YahooWoeID,GooglePlaceID
-';
-pdoStmtToJson(getDataByID($connection, $id,"GalleryID", $paramList, 'art.Galleries'));
-
-
-
-$connection = null;
-?>
-
-
-
-
 <div class="row">
 
     <div id="artist-single" class="three columns">
