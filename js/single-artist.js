e68b27261b3181a52b8adb9e0cfd0283cbde719b
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-08 14:46:41 -0700

    single artist js made and added

---
+++ js/single-artist.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', main);
+
+let artistID = 0;
+
+function main() {
+    setArtistID();
+    doWebCall(`./services/painting.php?artist=${artistID}`, addPaintings)
+}
+
+function setArtistID(){
+    artistID = getCurrentID();
+}
+
+function addPaintings(response) {
+    const elem = document.querySelector('#painting-table > table > tbody');
+    response.forEach((curr)=>{
+        const tr = document.createElement('tr');
+        let img = document.createElement('img');
+        img.setAttribute('src', `make-image.php?type=paintings&file=${curr['ImageFileName']}`);
+        img.setAttribute('alt', curr['Title']);
+        img=makeTD(img, 'growable');
+        const title = makeTD(curr['Title']);
+        const year = makeTD(curr['YearOfWork']);
+        doWebCall(`./services/artist.php?id=${curr['ArtistID']}`, (response)=>{
+            // this is where stuff get's appended
+            // need to query the artist name since that dose not come with the painting info
+            const artist = makeTD(`${response['FirstName']} ${response['LastName']}`);
+            tr.appendChild(img);
+            tr.appendChild(title);
+            tr.appendChild(artist);
+            tr.appendChild(year);
+            elem.appendChild(tr);
+        })
+    });
+}
