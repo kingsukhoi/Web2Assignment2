7306b8432cb2068f218e978a59e859449556195f
gio <gio@macmoro.com>
2018-12-08 16:53:33 -0700

    carrousel file- Not working.

--- js/index.js
+++ js/index.js
@@ -87,23 +87,3 @@ function addGalleries() {
         })
     }
 }
-
-var carrouselopts = {
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    rows: 2, // Removes the linear order. Would expect card 5 to be on next row, not stacked in groups.
-    responsive: [
-        { breakpoint: 992,
-            settings: {
-                slidesToShow: 3
-            }
-        },
-        { breakpoint: 776,
-            settings: {
-                slidesToShow: 1,
-                rows: 1 // This doesn't appear to work in responsive (Mac/Chrome)
-            }
-        }]
-};
-
-$('.carousel').slick(carrouselopts);
