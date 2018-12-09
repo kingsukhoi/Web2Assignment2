e1b5ea1e757547003074db922719810cf4c86a25
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-08 21:48:26 -0700

    indentation

--- index.php
+++ index.php
@@ -5,40 +5,40 @@
 <!---->
 
 <body>
-<?php include 'components/nav.php';
-generateNavBar() ?>
+    <?php include 'components/nav.php';
+        generateNavBar() ?>
 
-<div id="gallery-list" class="row dropdown">
-    <div class="twelve columns ">
-        <h2>Galleries</h2>
-        <ul class="dropdown-content">
-            <img src="images/Blocks-1s-200px.gif" alt="Loading">
-        </ul>
-        <!--<p>we will use a toggle to hide and show the list, will cover the page</p>-->
+    <div id="gallery-list" class="row dropdown">
+        <div class="twelve columns ">
+            <h2>Galleries</h2>
+            <ul class="dropdown-content">
+                <img src="images/Blocks-1s-200px.gif" alt="Loading">
+            </ul>
+            <!--<p>we will use a toggle to hide and show the list, will cover the page</p>-->
 
-    </div>
-</div>
-<div class="row">
-
-    <div id="artist" class="eight columns">
-        <h1>Artist</h1>
-
-        <div class="owl-carousel owl-theme">
-<!--            <img src="images/Blocks-1s-200px.gif" alt="Loading">-->
         </div>
-
     </div>
-    <div id="Genres" class="four columns">
-        <h1>Genres</h1>
-        <div class="row">
-            <div id="genres-list" class="two column">
-                <ul>
-                    <img src="images/Blocks-1s-200px.gif" alt="Loading">
-                </ul>
+    <div class="row">
+
+        <div id="artist" class="eight columns">
+            <h1>Artist</h1>
+
+            <div class="slider1">
+                <!--            <img src="images/Blocks-1s-200px.gif" alt="Loading">-->
+            </div>
+
+        </div>
+        <div id="Genres" class="four columns">
+            <h1>Genres</h1>
+            <div class="row">
+                <div id="genres-list" class="two column">
+                    <ul>
+                        <img src="images/Blocks-1s-200px.gif" alt="Loading">
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 <?php include 'inc/footer.inc.php' ?>
 </html>
