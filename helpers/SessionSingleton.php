4b6524f4c029a7cccb4f5ef57057699adf446a5d
Farsos Bulsara <kingsukhoi@gmail.com>
2018-12-09 19:23:17 -0700

    removed it

--- helpers/SessionSingleton.php
+++ helpers/SessionSingleton.php
@@ -6,8 +6,6 @@
  * Time: 9:27 PM
  */
 
-include "../db/db_helper.php";
-
 class Session_Singleton
 {
     /**
@@ -52,14 +50,6 @@ class Session_Singleton
     static function AddToFavorites(int $paintingID)
     {
         if (self::SessionStarted()) {
-            $pdo = newConnection();
-            $stmt = $pdo-> prepare(
-'SELECT PaintingID
-FROM Paintings
-WHERE PaintingID = :id;');
-            $stmt -> execute([':id'=>$paintingID]);
-            if ($stmt -> rowCount() == 0)
-                return false;
             if (!in_array($paintingID, $_SESSION[self::$FAVORITES_KEY]))
                 $_SESSION[self::$FAVORITES_KEY][] = $paintingID;
             return true;
